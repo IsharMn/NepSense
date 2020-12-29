7d913e90db6bf54e1eb054f253db6ee3ec6153c5
Ishar Maharjan <isharmaharjan123@gmail.com>
2020-12-29 19:01:00 +0545

    Floorsheet and CompanyList models created

--- backend/market/models.py
+++ backend/market/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 # Create your models here.
 class Stock(models.Model):
     company = models.CharField(max_length=250)
+    symbol = models.CharField(max_length=25, null=True)
     transno = models.PositiveIntegerField()
     maxp = models.PositiveIntegerField()
     minp = models.PositiveIntegerField()
@@ -18,3 +19,24 @@ class Stock(models.Model):
     def __str__(self):
         return str(self.company)
     
+
+class FloorSheet(models.Model):
+    contractnum = models.BigIntegerField()
+    symbol = models.CharField(max_length=25)
+    buyerbroker = models.IntegerField()
+    sellerbroker = models.IntegerField()
+    quantity = models.IntegerField()
+    rate = models.IntegerField()
+    amount = models.FloatField()
+    date_saved = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.contractnum)
+
+class CompanyList(models.Model):
+    name = models.CharField(unique=True, max_length=100, blank=True, null=True)
+    symbol = models.CharField(unique=True, max_length=20, blank=True, null=True)
+    sector = models.CharField(max_length=80, blank=True, null=True)
+
+    class Meta:
+        db_table = 'company_list'
